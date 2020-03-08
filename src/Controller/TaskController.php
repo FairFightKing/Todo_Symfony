940b1dd8636429e07efbfaa85243ae25a045d1b1
FairFightKIng <sosthenefruchard@gmail.com>
2020-03-08 16:27:17 +0100

    Task page + add form done

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,18 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class TaskController extends AbstractController
 {
     /**
      * @Route("/tasks", name="tasks")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+        $pdo = $this->getDoctrine()->getManager();
+        $tasks = $pdo->getRepository(Task::class)->findAll();
+
+        $task = new Task();
+        $form = $this->createForm(TaskType::class,$task);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $pdo->persist($task);
+            $pdo->flush();
+        }
+
+        return $this->render('task/index.html.twig',[
+            'tasks' => $tasks,
+            'add_task_form' => $form->createView()
         ]);
     }
     /**
--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -3,7 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Task;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +18,15 @@ class TaskType extends AbstractType
         $builder
             ->add('name')
             ->add('deadline')
-            ->add('status')
-            ->add('user')
+            ->add('status',CheckboxType::class,[
+                'label' => 'is this task done ?',
+                'required' => false
+            ])
+            ->add('user',EntityType::class, array(
+                'class' => User::class,
+                'choice_label' => 'name',
+            ))
+            ->add('save',SubmitType::class)
         ;
     }
 
